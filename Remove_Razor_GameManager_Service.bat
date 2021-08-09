ee29b823b9763c90be2cd819caf6c5cd75a31f99
Dabbles in too many things <mitch.zais@gmail.com>
2021-08-08 22:53:11 -0700

    Add files via upload

---
+++ Remove_Razor_GameManager_Service.bat
@@ -0,0 +1,7 @@
+reg delete "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Razer Synapse Service" /v DependOnService
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Razer Synapse Service" /v DependOnService /t Reg_Sz /d RzActionSvc
+sc config "Razer Synapse Service" depend=RzActionSvc
+sc stop "Razer Game Manager Service"
+sc config "Razer Game Manager Service" start=disabled
+sc start "Razer Synapse Service"
+pause 
